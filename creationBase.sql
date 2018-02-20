92f1a6123a44db9863b3ba05e8a62e31132b50da
Faller <antoine@MacBook-Air-de-Faller.local>
2018-02-20 10:35:12 +0100

    ajout de la base de donnée

---
+++ creationBase.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE ydays;
+
+USE ydays;
+
+CREATE TABLE user
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    login VARCHAR(100),
+    password VARCHAR(100),
+    ip_centreon VARCHAR(100),
+    login_centreon VARCHAR(255),
+    password_centreon VARCHAR(255)
+   
+);
+
+INSERT INTO user (login, password, ip_centreon, login_centreon, password_centreon)
+ VALUES
+ ('ydays', 'Ydays2018', '188.166.65.62','admin','Ydays2018' );
+
+ 
