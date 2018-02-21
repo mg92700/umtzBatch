b9224ededd7f93f4b81b3c7276ba7a4abbe82f37
Faller <antoine@MacBook-Air-de-Faller.local>
2018-02-21 14:53:19 +0100

    modification du fichiers de test

--- src/test/java/com/umtz/centreon/CentreonWebServiceApplicationTests.java
+++ src/test/java/com/umtz/centreon/CentreonWebServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.echosens.dashboard;
+package com.umtz.centreon;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DashBoardWebServiceApplicationTests {
+public class CentreonWebServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
